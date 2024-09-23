272b2c722222d503a318d2ca32c7e19d32117f3f
shotime369 <143595834+shotime369@users.noreply.github.com>
2024-09-23 18:54:20 +0100

    l;kjhv

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,43 @@
 public class Calculator {
 
-}
+        private final double num1;
+        private final double num2;
+        private final String operator;
+
+        //constructor
+    public Calculator(double num1, double num2, String operator) {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.operator = operator;
+        }
+
+        public double operate(){
+            if (this.operator.equals("-")) {
+                return num1 - num2;
+            } else if (this.operator.equals("+")) {
+                return num1 + num2;
+            } else if (this.operator.equals("x")) {
+                return num1 * num2;
+            } else if (this.operator.equals("/")) {
+                return num1 / num2;
+            } else if (this.operator.equals("%")) {
+                return num1 % num2;
+            } else { //if unknown return sum
+                return num1 + num2;
+            }
+        }
+
+    }
+
+
+
+
+  //  return switch (this.operator) {
+  //      case "-" -> num1 - num2;
+   //             case "+" -> num1 + num2;
+  //              case "x" -> num1 * num2;
+  //              case "/" -> num1 / num2;
+  //              case "%" -> num1 % num2;
+//default ->  //if unknown return sum
+//num1 + num2;
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1+1= " + new Calculator(1,1,"+").operate());
+        System.out.println("1-2= " + new Calculator(1,2,"-").operate());
+        System.out.println("1x3= " + new Calculator(1,3,"*").operate());
+        System.out.println("1/4= " + new Calculator(1,4,"/").operate());
+        System.out.println("1%5= " + new Calculator(1,5,"%").operate());
 
     }
 }
